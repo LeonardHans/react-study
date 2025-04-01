422850d81c132d2ff843ed09ba2ef533b38dd3e4
leonard <hans8114@gmail.com>
2025-04-02 00:29:51 +0900

    update border

--- src/component/Box.js
+++ src/component/Box.js
@@ -9,6 +9,9 @@ const Box = (props) => {
     else if (props.status == 'Lose') {
         border = '5px solid red';
     }
+    else if (props.status == 'Draw') {
+        border = '5px solid white';
+    }
 
     return (
         <div className="box" style={{ border }}>
