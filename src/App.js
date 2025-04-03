0a3052801619b0feabdae3a608157745996e16dd
leonard <hans8114@gmail.com>
2025-04-04 01:02:26 +0900

    update weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Box from './component/Box';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const apiKey = '8bb47e4ae02f68dc8ea8910a478e49a4'
 
 function App() {
-  return (
-  <div> START </div>
-  )
+
+  const [data, setData] = useState(null);
+  const cities = ['paris', 'new york', 'tokyo'];
+
+  useEffect( () => { /* onMount */
+    updateCurrent()
+  }, [] )
+
+  const updateCurrent = () => {
+    navigator.geolocation.getCurrentPosition( async (pos) => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${apiKey}&units=metric`
+      const res = await fetch(url)
+      const data = await res.json();
+      setData(data);
+    })
+  }
+
+  const updateOther = async (location) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+    const res = await fetch(url)
+    const data = await res.json();
+    setData(data);
+  }
+
+  return (<div className="main"> 
+      <div className='vertical-layout'>
+        <Box data={data}></Box>
+        <div className='horizontal-layout'>
+          <Button variant="primary" onClick={ () => updateCurrent() }>Current Location</Button>
+          {cities.map( (city) => (
+            <Button variant="info" onClick={ () => updateOther(city) }>{city}</Button>
+          ))}
+        </div>
+      </div>
+
+  </div>)
 }
 
 export default App;
---
+++ src/component/Box.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Box = (props) => {
+    const fahrenheit = Math.round(props.data?.main.temp*9/5 + 32)
+    return (
+        <div className="box">
+            <div style={{ fontSize: '30px' }}>{props.data?.weather[0].description}</div>
+            <p></p>
+            <div style={{ fontWeight: 'bold', color: 'orange', fontSize: '20px' }}>{props.data?.name}</div>
+            <p></p>
+            <div style={ props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{props.data && props.data?.main.temp}°C</div>
+            <div style={ props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{fahrenheit}°F</div>
+        </div>
+    )
+}
+
+export default Box
