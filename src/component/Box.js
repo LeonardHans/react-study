003c74b45e920dfaf5b20f9597f897d29bb3cddb
leonard <hans8114@gmail.com>
2025-04-06 01:36:42 +0900

    Update Box.js

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 const Box = (props) => {
-    const fahrenheit = Math.round(props.data?.main.temp*9/5 + 32)
+    let fahrenheit;
+    if (props.data?.main.temp) {
+        fahrenheit = (props.data?.main.temp * 9 / 5 + 32).toFixed(2);
+    }
     return (
         <div className="box">
             <div style={{ fontSize: '40px' }}>{props.data?.weather[0].description}</div>
             <p></p>
             <div style={{ fontWeight: 'bold', color: 'orange', fontSize: '20px' }}>{props.data?.name}</div>
             <p></p>
-            <div style={ props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{props.data && props.data?.main.temp}°C</div>
-            <div style={ props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{fahrenheit}°F</div>
+            <div style={props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{props.data && props.data?.main.temp}°C</div>
+            <div style={props.data?.main.temp < 10 ? { color: 'blue' } : { color: 'firebrick' }}>{fahrenheit}°F</div>
         </div>
     )
 }
