096142c06363f4b65d2e1b2e113fc3017cc21516
leonard <hans8114@gmail.com>
2025-04-13 21:00:46 +0900

    Flush all

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
-import { useEffect } from "react";
-import CounterBox from "./component/CounterBox";
-import counterStore from "./store/counterStore";
-
 function App() {
-  const { target, initTarget, score } = counterStore();
-
-  useEffect(() => {
-    initTarget();
-  }, []);
-
-  return (<div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-    <h1>Simple Count Setting Game</h1>
-    <p>All you have to set the count to the target by using buttons</p>
-    <h1>Target : {target}</h1>
-    <h1>Score : {score}</h1>
-    <CounterBox />
+  return (<div>
+    Empty
   </div>);
 }
 
