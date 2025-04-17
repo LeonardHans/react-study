9c863eee01d1a77bebff3daa5ac19d8c9bd541e2
leonard <hans8114@gmail.com>
2025-04-17 20:27:51 +0900

    clear

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { Button, Grid } from "@mui/material";
-import ContactForm from "./component/ContactForm";
-import ContactList from "./component/ContactList";
-
 function App() {
-  return (<div style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-    <h1>Contact Book</h1>
-    <Button variant="contained" onClick={ () => window.location.reload() }>Reset Book</Button>
-    <div style={{ width: "100%", display: "flex", flexDirection: "column", justifyContent: "center"}}>
-      <Grid container spacing={2}>
-        <Grid size={6}>
-          <ContactForm />
-        </Grid>
-        <Grid size={6}>
-          <ContactList />
-        </Grid>
-      </Grid>
-    </div>
+  return (<div>
   </div>);
 }
 
